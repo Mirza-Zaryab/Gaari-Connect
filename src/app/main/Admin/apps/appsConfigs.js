fb95b1483bd6b383c24f08cf154508c4c6c3fb8c
Zaryab <mirzazaryab07@gmail.com>
2024-08-31 23:54:09 +0500

    StateSlice and AppConfig

---
+++ src/app/main/Admin/apps/appsConfigs.js
@@ -0,0 +1,43 @@
+import AcademyAppConfig from './academy/AcademyAppConfig';
+import CalendarAppConfig from './calendar/CalendarAppConfig';
+import ChatAppConfig from './chat/ChatAppConfig';
+import ContactsAppConfig from './contacts/ContactsAppConfig';
+import AnalyticsDashboardAppConfig from './dashboards/analytics/AnalyticsDashboardAppConfig';
+import ProjectDashboardAppConfig from './dashboards/project/ProjectDashboardAppConfig';
+import ECommerceAppConfig from './e-commerce/ECommerceAppConfig';
+import FileManagerAppConfig from './file-manager/FileManagerAppConfig';
+import MailAppConfig from './mail/MailAppConfig';
+import NotesAppConfig from './notes/NotesAppConfig';
+import ScrumboardAppConfig from './scrumboard/ScrumboardAppConfig';
+import TodoAppConfig from './todo/TodoAppConfig';
+import VehiclesAppConfig from './Vehicle/VehiclesAppConfig';
+import UsersAppConfig from './User/UsersAppConfig';
+import StaffsAppConfig from './Staff/StaffsAppConfig';
+
+
+
+const appsConfigs = [
+  AnalyticsDashboardAppConfig,
+  ProjectDashboardAppConfig,
+  MailAppConfig,
+  TodoAppConfig,
+  FileManagerAppConfig,
+  ContactsAppConfig,
+  CalendarAppConfig,
+  ChatAppConfig,
+  ECommerceAppConfig,
+  ScrumboardAppConfig,
+  AcademyAppConfig,
+  NotesAppConfig,
+  VehiclesAppConfig,
+  UsersAppConfig,
+  StaffsAppConfig,
+ 
+  
+ 
+  
+  
+ 
+];
+
+export default appsConfigs;
