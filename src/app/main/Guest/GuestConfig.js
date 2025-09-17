2b74f548ea16543b3b1f9a82c95a63f2a9f25c9d
Zaryab <mirzazaryab07@gmail.com>
2025-09-17 23:24:09 +0500

    GuestConfig File

---
+++ src/app/main/Guest/GuestConfig.js
@@ -0,0 +1,9 @@
+import GuestPanelConfig from './guestPanel/GuestPanelConfig';
+import GuestMenuConfig from './menu/GuestMenuConfig';
+
+const GuestConfig = [
+    GuestPanelConfig,
+    GuestMenuConfig,
+];
+
+export default GuestConfig;
