6bb2a90e89425114e3f754db4a868cadef39cfe4
Zaryab <mirzazaryab07@gmail.com>
2025-10-13 01:34:22 +0500

    HomeFile

---
+++ src/app/main/Home/homeConfigs.js
@@ -0,0 +1,37 @@
+import LandingPageAppConfig from "./LandingPage/LandingPageAppConfig";
+import SignInConfigs from "./SignIn/SignInConfigs";
+import forgetpasswordConfig from "./forgetpassword/forgetpasswordConfig";
+import VerifyConfig from "./Verify/VerifyConfig";
+import SetpasswordConfig from "./Setpassword/SetpasswordConfig";
+import SignupConfig from "./Signup/SignupConfig";
+import VerifyaccountConfig from "./Verifyaccount/VerifyaccountConfig";
+import CreatepasswordConfig from "./Createpassword/CreatepasswordConfig";
+import OptionConfig from "./Option/OptionConfig";
+import OtpOptionConfig from "./otpOption/OtpOptionConfig";
+import PaymentConfig from "./Payment/PaymentConfig";
+import CarddetailConfig from "./Carddetail/CarddetailConfig";
+import ComingsoonConfig from "./Comingsoon/ComingsoonConfig";
+import GifConfig from "./Gif/GifConfig";
+import ConfirmandpayConfig from "./Confirmandpay/ConfirmandpayConfig";
+import ReceiptConfig from "./Receipt/ReceiptConfig";
+
+
+const homeConfigs = [
+    LandingPageAppConfig,
+    SignInConfigs,
+    forgetpasswordConfig,
+    VerifyConfig,
+    SetpasswordConfig,
+    SignupConfig,
+    VerifyaccountConfig,
+    CreatepasswordConfig,
+    OptionConfig,
+    OtpOptionConfig,
+    PaymentConfig,
+    CarddetailConfig,
+    ComingsoonConfig,
+    GifConfig,
+    ConfirmandpayConfig,
+    ReceiptConfig,
+];
+export default homeConfigs;
