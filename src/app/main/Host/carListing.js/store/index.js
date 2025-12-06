d3044b0a7d08ed8bc4bfa04be4464ed88b58bcf2
Zaryab <mirzazaryab07@gmail.com>
2025-12-06 23:59:39 +0500

    ID File

---
+++ src/app/main/Host/carListing.js/store/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import CarsListingSlice from "./CarsListingSlice";
+
+const reducer =  combineReducers({
+    reducer: {
+       CarsListingSlice
+    }
+});
+
+export default reducer;
